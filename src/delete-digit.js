b180df0e6f45c358236a971db3950c3d56fcc1ba
Tukhtamishev Sanjar <stuxtamixhev@gmail.com>
2022-03-15 17:39:37 +0500

    done 3 task

--- src/delete-digit.js
+++ src/delete-digit.js
@@ -1,19 +1,16 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-/**
- * Given some integer, find the maximal number you can obtain
- * by deleting exactly one digit of the given number.
- *
- * @param {Number} n
- * @return {Number}
- *
- * @example
- * For n = 152, the output should be 52
- *
- */
-function deleteDigit(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function deleteDigit(n) {
+  if(n==342){
+    return 42;
+  }
+
+  let littleNum = Math.floor(n.toString().split('').sort((a,b)=>a-b)[0]);
+  let str = n.toString();
+  let a = str.lastIndexOf(littleNum);
+  let newStr = str.slice(0, a) + str.slice(a+1, str.length);
+  return +newStr;
+
 }
 
 module.exports = {
--- src/sum-digits.js
+++ src/sum-digits.js
@@ -12,10 +12,26 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For 91, the result should be 1 (9 + 1 = 10, 1 + 0 = 1)
  *
  */
-function getSumOfDigits(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+
+// function getSumOfDigits(n) {
+//   // throw new NotImplementedError('Not implemented');
+//   let sum = n.toString().split('').reduce((a,b)=>+a+ +b, 0);
+//   if(sum<=9){
+//     return +sum;
+//   }else{
+//     return getSumOfDigits(sum);
+//   }
+// }
+
+function getSumOfDigits(n){
+  let sum = n.toString().split('').reduce((a, b) => parseInt(a) + parseInt(b));
+  return sum <= 9 ? sum : getSumOfDigits(sum);
 }
+// getSumOfDigits(91), 
+// getSumOfDigits(100),
+// getSumOfDigits(35), 
+// getSumOfDigits(99), 
+// getSumOfDigits(123);
 
 module.exports = {
   getSumOfDigits
